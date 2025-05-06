9455360732af31a7ab7ce2292794ea1972f4c703
yannick-itoua <mangeyannick@gmail.com>
2025-05-05 21:47:38 -0600

    deployment learning

--- easyaccess-frontend/src/app/utils/api.ts
+++ easyaccess-frontend/src/app/utils/api.ts
@@ -22,7 +22,7 @@ export interface User {
   role: string;
 }
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 
 // --- Location APIs ---
 
